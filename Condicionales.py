683d0162abc897990be22b8af72273996d3ddb7a
JuanReynoso-JR <ceinnova10624@inovaccion.mx>
2022-08-07 20:02:42 -0500

    inicial commit

---
+++ Condicionales.py
@@ -0,0 +1,8 @@
+calificacion = int(input("Ingresa tu calificación en el AZ-900: "))
+if calificacion < 700 :
+    print ("Sigue intentando")
+elif calificacion > 1000 :
+    print ("Naaaaaaaaaaaaa, mientes mas que tu ex, el maximo son 1000 puntos")
+else :
+    print ("Felicidades")
+    print ("Pasaste :) ")
