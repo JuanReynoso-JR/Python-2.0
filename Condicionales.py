18598c87dd80389d2b8265d597f87eea988971fd
JuanReynoso-JR <ceinnova10624@innovaccion.mx>
2022-08-07 20:42:08 -0500

    Condicional aprovatoria

--- Condicionales.py
+++ Condicionales.py
@@ -1,6 +1,8 @@
 calificacion = int(input("Ingresa tu calificación en el AZ-900: "))
 if calificacion < 700 :
     print ("Sigue intentando")
+elif calificacion == 700 :
+    print ("Panzaste")
 elif calificacion > 1000 :
     print ("Naaaaaaaaaaaaa, mientes mas que tu ex, el maximo son 1000 puntos")
 else :
